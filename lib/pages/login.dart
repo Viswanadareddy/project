c2e2bd24a41b9156b5b27bdbeea5b732363fb468
ViswanadaReddy Indupuru <viswanadareddyindupuru@gmail.com>
2021-10-07 02:19:16 +0530

    login page updated

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,79 +12,104 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           SizedBox(height: 400),
           Center(
-              child: Text(
-            'WELCOME BACK!',
-            style: TextStyle(
-                fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
-          )),
+            child: Text(
+              'WELCOME BACK!',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
           Center(
-              child: TextField(
-                  style: TextStyle(color: Colors.black),
-                  decoration: InputDecoration(
-                    prefixIcon: Container(
-                      child: Icon(
-                        Icons.email_outlined,
-                        color: Colors.white,
-                      ),
-                      //color: Colors.blue,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlueAccent,
-                        border: Border.all(width: 0.1),
-                        borderRadius: BorderRadius.circular(0.1),
-                      ),
-                    ),
-                    hintText: 'Email Adress',
-                    fillColor: Colors.white70,
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                            bottomLeft: Radius.circular(5),
-                            bottomRight: Radius.circular(5))),
-                  ))),
+            child: TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                prefixIcon: Container(
+                  child: Icon(
+                    Icons.email_outlined,
+                    color: Colors.white,
+                  ),
+                  //color: Colors.blue,
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    border: Border.all(width: 0.1),
+                    borderRadius: BorderRadius.circular(0.1),
+                  ),
+                ),
+                hintText: 'Email Adress',
+                fillColor: Colors.white70,
+                filled: true,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(5),
+                        topRight: Radius.circular(5),
+                        bottomLeft: Radius.circular(5),
+                        bottomRight: Radius.circular(5))),
+              ),
+            ),
+          ),
           SizedBox(height: 10),
           Center(
-              child: TextField(
-                  style: TextStyle(color: Colors.black),
-                  decoration: InputDecoration(
-                    prefixIcon: Container(
-                      child: Icon(
-                        Icons.lock_outline_rounded,
-                        color: Colors.white,
-                      ),
-                      //color: Colors.blue,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlueAccent,
-                        border: Border.all(width: 0.1),
-                        borderRadius: BorderRadius.circular(0.1),
-                      ),
-                    ),
-                    hintText: 'Password',
-                    fillColor: Colors.white70,
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                            bottomLeft: Radius.circular(5),
-                            bottomRight: Radius.circular(5))),
-                  ))),
+            child: TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                prefixIcon: Container(
+                  child: Icon(
+                    Icons.lock_outline_rounded,
+                    color: Colors.white,
+                  ),
+                  //color: Colors.blue,
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    border: Border.all(width: 0.1),
+                    borderRadius: BorderRadius.circular(0.1),
+                  ),
+                ),
+                hintText: 'Password',
+                fillColor: Colors.white70,
+                filled: true,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(5),
+                        topRight: Radius.circular(5),
+                        bottomLeft: Radius.circular(5),
+                        bottomRight: Radius.circular(5))),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
           Text(
             'Forgot password',
             style: TextStyle(fontSize: 14, color: Colors.white),
-            textAlign: TextAlign.left,
+            textAlign: TextAlign.right,
           ),
-          Text(
-            'I haven\'t an account ',
-            style: TextStyle(
-                fontSize: 14,
-                decoration: TextDecoration.underline,
-                color: Colors.white),
-            textAlign: TextAlign.center,
+          Center(
+            child: SizedBox(
+              width: 350,
+              child: RawMaterialButton(
+                onPressed: () {},
+                padding: EdgeInsets.all(10),
+                child: Text('LOGIN'),
+                fillColor: Colors.blue,
+                textStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              'I haven\'t an account ',
+              style: TextStyle(
+                  fontSize: 14,
+                  decoration: TextDecoration.underline,
+                  color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
           ),
         ],
       ),
