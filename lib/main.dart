76e86baf995fb04a36a3b62b36c09829c1735faa
ViswanadaReddy Indupuru <viswanadareddyindupuru@gmail.com>
2021-10-10 19:28:00 +0530

    removed unnecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:project/pages/payment.dart';
 import 'package:project/pages/register.dart';
 import 'package:project/pages/setting.dart';
 import 'package:project/pages/profile.dart';
-import 'package:project/unbording/MyHomePage.dart';
+import 'package:project/onbording/MyHomePage.dart';
 import 'package:project/widgets/bottomNavigationBar.dart';
 import 'package:project/pages/customisation.dart';
 import 'package:project/pages/cart.dart';
@@ -40,8 +40,6 @@ class MyApp extends StatelessWidget {
           'menu': (context) => MenuPage(),
           'customisation': (context) => Customisation(),
           'cartpage': (context) => CartPage(),
-          //'products': (context) => ProductListPage(),
-          //'products_detail': (context) => ProductDetailsPage(),
         },
         theme: ThemeData(
             primarySwatch: Colors.blue,
@@ -64,66 +62,3 @@ class MyApp extends StatelessWidget {
                 bodyText2: TextStyle(fontSize: 20.0, color: Colors.white))),
       );
 }
-
-/*class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
- 
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-  
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
-}*/
-
-/*class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Timer(Duration(milliseconds: 5000), () {
-      Navigator.pushNamed(context, 'setting');
-    });
-    return Container();
-  }
-}*/
