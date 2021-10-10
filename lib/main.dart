1f87941a8067c49ab570e8fc8f3d748d36481b0f
ViswanadaReddy Indupuru <viswanadareddyindupuru@gmail.com>
2021-10-10 15:56:38 +0530

    check project

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        home: Customisation(),
+        home: MyHomePage(),
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         //themeMode: ThemeMode.dark,
